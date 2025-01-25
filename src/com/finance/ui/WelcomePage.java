d65cdeb79b2c427292c97514a7f8ef84d9e2f596
Ashish <chavanashish4147@gmail.com>
2025-01-25 06:48:54 +0530

    Created a Welcome message in WelcomePage.java

--- src/com/finance/ui/WelcomePage.java
+++ src/com/finance/ui/WelcomePage.java
@@ -12,8 +12,16 @@ public class WelcomePage extends JFrame {
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(null); // set Frame to center
 
+        //creating a Welcome Message
+        JLabel WelcomeLabel1 = new JLabel("Welcome to the Personal Finance Manager",SwingConstants.CENTER);
+
+
+
+
+
+        add(WelcomeLabel1);
         // Set the frame visible
         setVisible(true);
     }
